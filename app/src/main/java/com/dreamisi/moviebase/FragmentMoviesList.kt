a3822e6b9cb1b878890c561fac12ddcf9808c285
dreamisi <dimanich03@yandex.ru>
2022-12-11 18:19:14 +0300

    in process of adding recycle view for movies list

--- app/src/main/java/com/dreamisi/moviebase/FragmentMoviesList.kt
+++ app/src/main/java/com/dreamisi/moviebase/FragmentMoviesList.kt
@@ -21,7 +21,7 @@ class FragmentMoviesList : Fragment(R.layout.fragment_movies_list) {
         recycler = view.findViewById(R.id.movies_cards)
         recycler?.adapter = MoviesListAdapter()
         recycler?.layoutManager = GridLayoutManager(requireContext(),2)
-        view.findViewById<ImageView>(R.id.movie_image)?.apply {//нужно переписать этот код, кликлистнере ссылается на нулевой объект теперь
+        view.findViewById<ImageView>(R.id.movie_image)?.apply {//нужно переписать этот код, кликлистенер ссылается на нулевой объект теперь
             setOnClickListener {
                 fragmentMoviesListListener?.onFilmCardClicked()
             }
