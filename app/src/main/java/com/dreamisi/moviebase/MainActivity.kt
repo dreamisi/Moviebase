34c78210a4a7a6fea504e3a889a6576aa5307cb2
Lazutin Dmitry <Dimanich03@yandex.ru>
2022-12-19 22:33:14 +0300

    start adding recycle view for actors bar

--- app/src/main/java/com/dreamisi/moviebase/MainActivity.kt
+++ app/src/main/java/com/dreamisi/moviebase/MainActivity.kt
@@ -12,7 +12,7 @@ class MainActivity : AppCompatActivity(), FragmentMoviesList.FragmentMoviesListL
 
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
-                .add(R.id.main_container, FragmentMoviesDetails())
+                .add(R.id.main_container, FragmentMoviesList())
                 .commit()
         }
 
